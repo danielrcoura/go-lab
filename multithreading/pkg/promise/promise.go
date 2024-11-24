84672e172edc7eb02f8373535be4164ead989d83
danielrcoura <danielcoura22@gmail.com>
2024-11-24 16:02:32 -0300

    add promise all

--- multithreading/pkg/promise/promise.go
+++ multithreading/pkg/promise/promise.go
@@ -2,8 +2,40 @@
 package promise
 
 import (
+	"context"
+	"errors"
 	"sync"
 )
+
+// All returns all the results of the promises.
+// If any of the promises fail, the function will imediately return.
+func All(funcs ...func() (any, error)) ([]any, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	wg := sync.WaitGroup{}
+	wg.Add(len(funcs))
+	defer wg.Wait()
+	results := make([]any, len(funcs))
+	for i, f := range funcs {
+		go func() {
+			data, err := f()
+			if err != nil {
+				cancel()
+				wg.Done()
+				return
+			}
+			results[i] = data
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	select {
+	case <-ctx.Done():
+		return results, errors.New("aborted")
+	default:
+		return results, nil
+	}
+}
 
 func AllSettled(funcs ...func() string) []string {
 	wg := sync.WaitGroup{}
--- multithreading/pkg/promise/promise_test.go
+++ multithreading/pkg/promise/promise_test.go
@@ -1,13 +1,26 @@
 package promise
 
 import (
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// Teste para a função Soma
+func TestAll(t *testing.T) {
+	result, _ := All(dummyFunc("f1", 100), dummyFunc("f2", 500), dummyFunc("f3", 1000))
+	assert.Equal(t, []any{"f1", "f2", "f3"}, result)
+}
+
+func TestAllError(t *testing.T) {
+	errorFn := func() (any, error) {
+		return "", errors.New("error")
+	}
+	_, err := All(errorFn, dummyFunc("f2", 500), dummyFunc("f3", 1000))
+	assert.Error(t, err, "Expected an error but got nil")
+}
+
 func TestAllSettled(t *testing.T) {
 	f1 := func() string {
 		time.Sleep(100 * time.Millisecond)
@@ -23,4 +36,11 @@ func TestAllSettled(t *testing.T) {
 	}
 	result := AllSettled(f1, f2, f3)
 	assert.Equal(t, []string{"f1", "f2", "f3"}, result)
+}
+
+func dummyFunc(name string, milliseconds int64) func() (any, error) {
+	return func() (any, error) {
+		time.Sleep(time.Duration(milliseconds) * time.Millisecond)
+		return name, nil
+	}
 }
